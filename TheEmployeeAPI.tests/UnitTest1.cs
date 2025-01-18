89556a75cd376145d964a2094827eab5aa0f743d
hazbob <hrobinson3781@gmail.com>
2025-01-18 12:13:54 +0000

    first commit

---
+++ TheEmployeeAPI.tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace TheEmployeeAPI.tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ TheEmployeeApi/Employee.cs
@@ -0,0 +1,7 @@
+namespace TheEmployeeApi;
+
+public class Employee {
+    public int Id { get; set; }
+    public required string FirstName { get; set; }  
+    public required string LastName { get; set; }
+}

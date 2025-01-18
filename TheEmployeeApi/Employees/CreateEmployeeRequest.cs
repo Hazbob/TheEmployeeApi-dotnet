9f6e3c694774c6107b5eec98c1a4423524508128
hazbob <hrobinson3781@gmail.com>
2025-01-18 19:49:34 +0000

    Remove debug folders from git tracking and add them to gitignore

---
+++ TheEmployeeApi/Employees/CreateEmployeeRequest.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TheEmployeeApi.Employees;
+
+public class CreateEmployeeRequest
+{
+    [Required(AllowEmptyStrings = false)]
+    public string? FirstName { get; set; }
+    [Required(AllowEmptyStrings = false)]
+    public string? LastName { get; set; }
+    public string? NationalInsuranceNumber { get; set; }
+
+    public string? Address1 { get; set; }
+    public string? Address2 { get; set; }
+    public string? City { get; set; }
+    public string? County { get; set; }
+    public string? PostCode { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Email { get; set; }
+}
---
+++ TheEmployeeApi/Employees/GetEmployeeResponse.cs
@@ -0,0 +1,15 @@
+namespace TheEmployeeApi.Employees;
+
+public class GetEmployeeResponse
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+
+    public string? Address1 { get; set; }
+    public string? Address2 { get; set; }
+    public string? City { get; set; }
+    public string? County { get; set; }
+    public string? PostCode { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Email { get; set; }
+}
